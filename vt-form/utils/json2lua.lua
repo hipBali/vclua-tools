943df2c364a59ac184005219762b4948d87a82dd
hipBali <107695893+hipBali@users.noreply.github.com>
2024-01-11 17:15:59 +0100

    Update json2lua.lua

--- vt-form/utils/json2lua.lua
+++ vt-form/utils/json2lua.lua
@@ -28,17 +28,19 @@ function json2lua(fileName)
 			table.insert(lua_out,(string.format("%s%s = '%s',",string.rep("\t",n),key,value)))
 		end
 	end
+	table.insert(lua_out,"local frm={}\n")
 	for n,c in pairs(frm) do
 		if VCL[c.class]~=nil then
-			table.insert(lua_out,(string.format("local %s = VCL.%s( %s, '%s', { ", c.name, c.class, tostring(c.parent), c.name)))
+			table.insert(lua_out,(string.format("frm.%s = VCL.%s( %s, '%s', { ", c.name, c.class, tostring(c.parent), c.name)))
 			for k,v in pairs(c.props) do
 				if string.lower(k)~="name" then
 					print_prop(k,v)
 				end
 			end
-			table.insert(lua_out,("})"))
+			table.insert(lua_out,"})")
 		end
 	end
+	table.insert(lua_out,"\nreturn frm")
 	return lua_out
 end
 
