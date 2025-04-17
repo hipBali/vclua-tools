a8cc7ee83c1183b0e3450eac99f543ce223f0a7c
roman-orekhov <roman.s.orekhov@gmail.com>
2025-04-18 00:52:01 +0300

    json2lua: update to current format

--- vt-form/utils/json2lua.lua
+++ vt-form/utils/json2lua.lua
@@ -1,52 +1,81 @@
 -- ***************************************
 -- Json form to lua script converter
--- Copyright (C) 2013-2024 Hi-Project Ltd.
+-- Copyright (C) 2013-2025 Hi-Project Ltd.
 -- ***************************************
 local VCL = require "vcl.core"
-local json = require "json"
 
-function json2lua(fileName) 
-    assert(fileName,"missing filename parameter!")
-	local file, errorString = io.open( fileName, mode or "r"  )
-	assert(file,string.format("%s not found!", tostring(fileName)))
-	local contents = file:read( "*a" )
-	io.close( file )
-	local frm = json.decode(contents)
-	local print_prop
-	local lua_out = {}
-	print_prop = function(key,value,n)
-		n = n or 1
-		if type(value)=="table" then
-			table.insert(lua_out,(string.format("%s%s = {",string.rep("\t",n),key)))
-			for k,v in pairs(value) do
-				print_prop(k,v, n+1)
-			end
-			table.insert(lua_out,(string.format("%s},",string.rep("\t",n),key)))
-		elseif type(value)=="string" then
-			table.insert(lua_out,(string.format("%s%s = '%s',",string.rep("\t",n),key,value)))
-		else
-			table.insert(lua_out,(string.format("%s%s = %s,",string.rep("\t",n),key,tostring(value))))
-		end
-	end
-	table.insert(lua_out,"local frm={}\n")
-	for n,c in pairs(frm) do
-		if VCL[c.class]~=nil then
-			local pname = c.parent
-			if pname then
-				pname = "frm."..pname
-			end
-			table.insert(lua_out,(string.format("frm.%s = VCL.%s( %s, '%s', { ", c.name, c.class, tostring(pname), c.name)))
-			for k,v in pairs(c.props) do
-				if string.lower(k)~="name" then
-					print_prop(k,v)
-				end
-			end
-			table.insert(lua_out,"})")
-		end
-	end
-	table.insert(lua_out,"\nreturn frm")
-	return lua_out
+require "loader"
+
+local function split(s)
+  local fields = {}
+  s:gsub("([^.]+)", function(c) fields[#fields+1] = c end)
+  return fields
 end
 
-local j2lua = json2lua(arg[1])
+function json2lua(fileName, unique)
+  assert(fileName,"missing filename parameter!")
+  local frm = fileToJson(fileName)
+  local function has_duplicate_names(c, names)
+    names = names or {}
+    if names[c.name] then
+      return true
+    end
+    names[c.name] = true
+    for _,c in ipairs(c.items) do
+      if has_duplicate_names(c, names) then return true end
+    end
+    return false
+  end
+  unique = unique == nil and not has_duplicate_names(frm) or (unique == "true")
+  local print_prop
+  local lua_out = {"by_name = {}"}
+  print_prop = function(key,value,n)
+    n = n or 1
+    key = type(key) == "number" and math.type(key) == "integer" and "" or string.format("%s = ", key)
+    if type(value)=="table" then
+      table.insert(lua_out,(string.format("%s%s{",string.rep("\t",n),key)))
+      for k,v in pairs(value) do
+        print_prop(k,v, n+1)
+      end
+      table.insert(lua_out,(string.format("%s},",string.rep("\t",n),key)))
+    elseif type(value)=="string" then
+      local words = split(value)
+      if table.remove(words) == 'vt-form' then
+        value = unique and table.remove(words) or table.concat(words, "_")
+      else
+        value = "'"..value.."'"
+      end
+      table.insert(lua_out,(string.format("%s%s%s,",string.rep("\t",n),key,value)))
+    else
+      table.insert(lua_out,(string.format("%s%s%s,",string.rep("\t",n),key,tostring(value))))
+    end
+  end
+
+  local function add_tree(c,path)
+    local p = path
+    path = unique and c.name or (path and path.."_" or "")..c.name
+    table.insert(lua_out, string.format('%s = VCL.%s(%s, "%s")', path, c.class, p, c.name))
+    table.insert(lua_out, string.format('by_name.%s = %s', path, path))
+    for _,c in ipairs(c.items) do add_tree(c,path) end
+  end
+
+  local function set_props(c,path)
+    path = unique and c.name or (path and path.."_" or "")..c.name
+    if c.props and next(c.props) then
+      table.insert(lua_out, string.format("%s._ = {", path))
+      for k,v in pairs(c.props) do
+        print_prop(k,v)
+      end
+      table.insert(lua_out,"}")
+    end
+    for _,c in ipairs(c.items) do set_props(c,path) end
+  end
+
+  add_tree(frm)
+  set_props(frm)
+
+  return lua_out
+end
+
+local j2lua = json2lua(arg[1], arg[2])
 print(table.concat(j2lua,"\n"))
